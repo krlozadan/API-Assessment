e8d36fc8dac73eee509417e6c189fe88b58846ef
Carlos Adan <krlozadan@gmail.com>
2019-03-19 21:40:57 -0600

    Establish connection to database

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-const config = require("./config");
-// Web framework package
 const express = require("express");
-//  Parse the incoming req body for easier manipulation
 const bodyParser = require("body-parser");
-// API enpoints
+const mongoose = require("mongoose");
+const config = require("./config");
+const PromiseWrapper = require("./models/promiseWrapper");
 const routes = require("./routes/routes");
 
+// Self invoking function to connect to the DB. Kills the node instance if it can't connect
+(async () => {
+    const wrapper = new PromiseWrapper();
+    const { error : errorConnectingToDB } = await wrapper.async(mongoose.connect(config.databaseURL, { dbName : config.databaseName, useNewUrlParser : true }));
+    if (errorConnectingToDB != null) {
+        console.log("Couldn't connect to Database");
+        process.exit();
+    }
+    console.log("Successfully connected to Database");
+})();
+
+// Web framework package
 // Instantiate the api instance and setup middleware and routes
 const api = express();
+//  Parse the incoming req body for easier manipulation
 api.use(bodyParser.json({ limit : config.bodyLimit }));
+// API enpoints
 api.use("/api", routes);
 
 // Start API
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,8 @@ const locationRoutes = require("./location");
 
 // Set up API routing here 
 const generalRouter = express.Router();
+
 generalRouter.use("/location", locationRoutes);
+
+
 module.exports = generalRouter;
