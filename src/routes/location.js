dc808522edde398e13b7106156250ccc36fadbca
Carlos Adan <krlozadan@gmail.com>
2019-03-19 20:44:08 -0600

    Create end point to retrieve and IP and return location info

--- src/routes/location.js
+++ src/routes/location.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const PromiseWrapper = require("../models/promiseWrapper");
+const iplocation = require("iplocation").default;
 
 const locationRouter = express.Router();
 
-locationRouter.get("/", (req, res) => {
-    res.status(200).json({message : "GET Location Route"});
+// GET
+// api/location/
+// Retrieve end user's remote IP address and detect user's location using 3rd party API
+locationRouter.get("/", async (req, res) => {
+    const wrapper = new PromiseWrapper();
+    const { data : location , error : errorGettingLocation } = await wrapper.async(iplocation(req.ip));
+    if (errorGettingLocation != null) {
+        res.status.apply(500).json({ error : "Something went wrong, please try again later" });
+    } else {
+        res.status(200).json({ location });
+    }
 });
 
 module.exports = locationRouter;
