b3962ceed2f4c251c4e53937629ec54d43dfac4c
Carlos Adan <krlozadan@gmail.com>
2019-03-19 21:58:33 -0600

    Create database user model

---
+++ src/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+// User Schema configuration
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true,
+        trim: true, 
+        match: /^[a-zA-Z ]{3,50}$/
+    },
+    last_name: {
+        type: String,
+        required: true,
+        trim: true,
+        match: /^[a-zA-Z ]{3,50}$/ 
+    },
+    ip_address : {
+        type: String,
+        required : true,
+        trim : true,
+        match : /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/
+    }
+}, { collection : 'users' });
+
+module.exports = mongoose.model('User', userSchema);
